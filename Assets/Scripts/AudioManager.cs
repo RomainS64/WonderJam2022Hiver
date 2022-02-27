1be34f2717b88527a3c7eee7f2943247c94b7df0
unknown <julen.leremboure@laposte.net>
2022-02-27 16:49:11 +0100

    sounds repaired

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -21,6 +21,10 @@ public class AudioManager : MonoBehaviour
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
-        s.source.Play();
+
+        if(s != null)
+        {
+            s.source.Play();
+        }
     }
 }
