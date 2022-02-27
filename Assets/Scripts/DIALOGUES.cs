2a7bc82db10a5195cc29ebcec3cff05a20ca29e9
Mortox-9 <76782611+Mortox-9@users.noreply.github.com>
2022-02-27 04:25:32 -0500

    dialogue

--- Assets/Scripts/DIALOGUES.cs
+++ Assets/Scripts/DIALOGUES.cs
@@ -8,31 +8,31 @@ static public class DIALOGUES
     static public string pieceVide = "Cette piece est vide... Je ne m'y sens pas très à l'aise...";
 
     ///PIECE DU CHIEN
-    static public string chienDocile1 = "Oh ! Un chien ! Il semble plutôt amical.";
+    static public string chienDocile1 = "Tu m'as l'air bien gentil petit CH1-3n. Qu'est-ce que tu fais icitte tout seul ?";
 
-    static public string chienDocileCaresse1 = "Ça fait vraiment du bien de le caresser.";
-    static public string chienDocileCaresse2 = "Ça fait vraiment du bien de le caresser. Il semble vouloir me suivre.";
+    static public string chienDocileCaresse1 = "Te câliner est réellement réconfortant.";
+    static public string chienDocileCaresse2 = "C'est un bon robot qui aime les caresses ça ! Oh, tu m'accompagnes ?";
 
-    static public string chienDocileAttaque1 = "AH ! Il m'a mordu !!";
-    static public string chienDocileAttaque2 = "Je n'aurais jamais imaginé faire ça un jour.";
+    static public string chienDocileAttaque1 = "Saleté de robot ! Il m'a frappé !";
+    static public string chienDocileAttaque2 = "Pourquoi ai-je fait ça, c'était même pas déstressant...";
 
-    static public string chienMechant1 = "Oh ! Un chien ! Il n'a pas l'air commode...";
+    static public string chienMechant1 = "Ce CH1-3n m'a vraiment l'air agressif...";
 
-    static public string chienMechantCaresse1 = "AH ! Il m'a mordu !!";
-    static public string chienMechantAttaque1 = "Il l'a mérité.";
+    static public string chienMechantCaresse1 = "AIE IL M'A FRAPPÉ ! J'aurais dû m'en douter aussi...";
+    static public string chienMechantAttaque1 = "Prends ça sale robot, c'est ça dégage !";
 
     ///PIECE SQUELETTE
-    static public string squeletteQuestion = "Ce squelette porte une armure. peut-être qu'elle pourra m'être utile.";
+    static public string squeletteQuestion = "L'armure de ce squelette me serait sûrement plus utile qu'à ce dernier...";
     static public string squeletteRep2 = "Prendre l'armure";
-    static public string squeletteRep1 = "Laisser l'armure";
+    static public string squeletteRep1 = "Laisser le cadavre";
 
-    static public string squelettePrendre = "C'est humide et très usé... mais ça devrait suffir pour me protéger.";
-    static public string squeletteLaisser = "Pas question que je touche à cette chose.";
+    static public string squelettePrendre = "Je suppose que je vais devoir faire avec l'odeur de putréfaction...";
+    static public string squeletteLaisser = "Je ferais mieux de laisser cette pauvre âme reposer en paix.";
 
     //TRESOR
     static public string tresorParTerre = "Il y a quelque chose par terre...";
 
-    static public string tresorRep1 = "Prendre";
+    static public string tresorRep1 = "Ramasser";
     static public string tresorRep2 = "Laisser";
 
     //Cristal
@@ -41,13 +41,13 @@ static public class DIALOGUES
         "un amas de chair remuer lentement en son centre.";
 
     static public string cristalTrouveEnabled =
-        "J'ai déjà vu ce cristal. Mais je ne me souviens pas d'une lumière aussi intense...";
+        "Encore ce cristal... J'ai l'impression qu'il émet une lumière plus intense.";
 
     static public string cristalToucheToEnable =
         "Ahh.. Ahh.. Ma tête... Je me sens... si faible...";
 
     static public string cristalToucheToDisable =
-        "HHHHHHHH !! AH ! Je... Je me sens rajeunit !";
+        "HHHHHHHH !! AH ! Je... Je me sens revitalisé !";
 
     static public string cristalRep1 = "Toucher";
     static public string cristalRep2 = "Ne pas toucher";
